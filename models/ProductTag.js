ad79af500392887474d6319039d8d8d418036139
Lauren Bleyl <laurenbleyl@gmail.com>
2023-08-13 22:02:51 -0500

    models and relationships set up

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
 
+
+
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      },
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      },
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
